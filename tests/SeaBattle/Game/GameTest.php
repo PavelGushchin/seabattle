99936be194e8cdd9ab68fc27ce5e80d798b2c315
Pavel Guschin <guschin@azoft.com>
2016-05-31 01:26:49 +0700

    Added tests for Game class

--- tests/SeaBattle/Game/GameTest.php
+++ tests/SeaBattle/Game/GameTest.php
@@ -18,11 +18,11 @@ class GameTest extends \PHPUnit_Framework_TestCase
      */
     public function testObjectCanBeConstructed()
     {
-        $g = new Game();
+        $game = new Game();
 
-        $this->assertInstanceOf('SeaBattle\\Game\\Game', $g);
+        $this->assertInstanceOf('SeaBattle\\Game\\Game', $game);
 
-        return $g;
+        return $game;
     }
 
     /**
@@ -30,9 +30,9 @@ class GameTest extends \PHPUnit_Framework_TestCase
      * @uses    \SeaBattle\Field\Field
      * @depends testObjectCanBeConstructed
      */
-    public function testMyFieldCanBeRetrieved(Game $g)
+    public function testMyFieldCanBeRetrieved(Game $game)
     {
-        $this->assertInstanceOf('SeaBattle\\Field\\Field', $g->getMyField());
+        $this->assertInstanceOf('SeaBattle\\Field\\Field', $game->getMyField());
     }
 
     /**
@@ -40,46 +40,141 @@ class GameTest extends \PHPUnit_Framework_TestCase
      * @uses    \SeaBattle\Field\Field
      * @depends testObjectCanBeConstructed
      */
-    public function testEnemyFieldCanBeRetrieved(Game $g)
+    public function testEnemyFieldCanBeRetrieved(Game $game)
     {
-        $this->assertInstanceOf('SeaBattle\\Field\\Field', $g->getEnemyField());
+        $this->assertInstanceOf('SeaBattle\\Field\\Field', $game->getEnemyField());
     }
 
     /**
      * @covers  \SeaBattle\Game\Game::isGameover
      * @depends testObjectCanBeConstructed
      */
-    public function testGameoverCanBeRetrieved(Game $g)
+    public function testGameoverCanBeRetrieved(Game $game)
     {
-        $this->assertSame(true, $g->isGameover());
+        $this->assertSame(true, $game->isGameover());
     }
 
     /**
      * @covers  \SeaBattle\Game\Game::getWinner
      * @depends testObjectCanBeConstructed
      */
-    public function testWinnerCanBeRetrieved(Game $g)
+    public function testWinnerCanBeRetrieved(Game $game)
     {
-        $this->assertSame(Game::NO_WINNER, $g->getWinner());
+        $this->assertSame(Game::NO_WINNER, $game->getWinner());
     }
 
     /**
      * @covers \SeaBattle\Game\Game::getTurn
      * @depends testObjectCanBeConstructed
      */
-    public function testTurnCanBeRetrieved(Game $g)
+    public function testTurnCanBeRetrieved(Game $game)
     {
-        $this->assertSame(Game::MY_TURN, $g->getTurn());
+        $this->assertSame(Game::MY_TURN, $game->getTurn());
     }
 
     /**
      * @covers \SeaBattle\Game\Game::passTurnToNextPlayer
      * @depends testObjectCanBeConstructed
      */
-    public function testTurnIsPassedToNextPlayer(Game $g)
+    public function testTurnIsPassedToNextPlayer(Game $game)
     {
-        $g->passTurnToNextPlayer();
+        $game->passTurnToNextPlayer();
 
-        $this->assertSame(Game::ENEMY_TURN, $g->getTurn());
+        $this->assertSame(Game::ENEMY_TURN, $game->getTurn());
+    }
+
+    /**
+     * @covers \SeaBattle\Game\Game::startNewGame
+     * @uses   \SeaBattle\Field\Field
+     */
+    public function testStartingNewGameCorrectDefaultsAreAssigned()
+    {
+        $game = new Game();
+        $game->startNewGame();
+
+        $this->assertSame(Game::NO_WINNER, $game->getWinner());
+        $this->assertSame(false, $game->isGameover());
+    }
+
+    /**
+     * Data provider for testWrongCoordsForShotsDoNotHitShips
+     *
+     * @return array
+     */
+    public function wrongCoordsProvider()
+    {
+        return [
+            [-1, -2],
+            [15, 25],
+            ["a", 5],
+            [6, "a"],
+            ["a", "b"],
+        ];
+    }
+
+    /**
+     * @covers \SeaBattle\Game\Game::shootingTo
+     * @covers \SeaBattle\Field\Field::handleShot
+     * @uses   \SeaBattle\Field\Field
+     * @uses   \SeaBattle\Game\Game::getEnemyField
+     *
+     * @dataProvider wrongCoordsProvider
+     */
+    public function testWrongCoordsForShotsDoNotHitShips($x, $y)
+    {
+        $game = new Game();
+        $game->startNewGame();
+
+        $enemyField = $game->getEnemyField();
+
+        $this->assertFalse($game->shootingTo($enemyField, $x, $y));
+    }
+
+    /**
+     * @covers \SeaBattle\Game\Game::setGameover
+     * @uses   \SeaBattle\Game\Game::isGameover
+     */
+    public function testGameoverCanBeSetCorrectly()
+    {
+        $game = new Game();
+
+        $game->setGameover(false);
+        $this->assertSame(false, $game->isGameover());
+
+        $game->setGameover(true);
+        $this->assertSame(true, $game->isGameover());
+    }
+
+    /**
+     * @covers \SeaBattle\Game\Game::setWinner
+     * @uses   \SeaBattle\Game\Game::getWinner
+     */
+    public function testWinnerCanBeSetCorrectly()
+    {
+        $game = new Game();
+
+        $game->setWinner(Game::I_AM_WINNER);
+        $this->assertSame(Game::I_AM_WINNER, $game->getWinner());
+
+        $game->setWinner(Game::ENEMY_IS_WINNER);
+        $this->assertSame(Game::ENEMY_IS_WINNER, $game->getWinner());
+
+        $game->setWinner(Game::NO_WINNER);
+        $this->assertSame(Game::NO_WINNER, $game->getWinner());
+    }
+
+    /**
+     * @covers \SeaBattle\Game\Game::setTurn
+     * @uses   \SeaBattle\Game\Game::getTurn
+     */
+    public function testTurnCanBeSetCorrectly()
+    {
+        $game = new Game();
+
+        $game->setTurn(Game::ENEMY_TURN);
+        $this->assertSame(Game::ENEMY_TURN, $game->getTurn());
+
+        $game->setTurn(Game::MY_TURN);
+        $this->assertSame(Game::MY_TURN, $game->getTurn());
     }
 }
