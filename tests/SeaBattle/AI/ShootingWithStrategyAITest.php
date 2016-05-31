33198dcfc37d1a62fbd8300eeba4422ca7f24e15
Pavel Guschin <guschin@azoft.com>
2016-06-01 00:25:16 +0700

    Added test for 'calculateCoordsForShooting' method

--- tests/SeaBattle/AI/ShootingWithStrategyAITest.php
+++ tests/SeaBattle/AI/ShootingWithStrategyAITest.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pavel
- * Date: 31.05.16
- * Time: 23:57
+
+/*
+ * This file is part of the SeaBattle package.
+ *
+ * (c) Pavel Gushchin <pavel_gushchin@mail.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
+
+namespace SeaBattle\AI;
+
+use SeaBattle\Field\Field;
+
+class ShootingWithStrategyAITest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \SeaBattle\AI\ShootingWithStrategyAI::calculateCoordsForShooting
+     * @uses   \SeaBattle\Field\Field::__construct
+     * @uses   \SeaBattle\Field\Field::createShips
+     * @uses   \SeaBattle\Field\Field::getSlots
+     * @uses   \SeaBattle\Field\Field::getShips
+     */
+    public function testReturnedCoordsAreWithinAppropriateRange()
+    {
+        $attackedField = new Field();
+        $attackedField->createShips();
+
+        $shootingAI = new ShootingWithStrategyAI();
+        $coordsForShooting = $shootingAI->calculateCoordsForShooting(
+            $attackedField->getSlots(),
+            $attackedField->getShips()
+        );
+
+        $x = $coordsForShooting['x'];
+        $y = $coordsForShooting['y'];
+
+        $this->assertTrue($x >= 0 && $x < Field::WIDTH);
+        $this->assertTrue($y >= 0 && $y < Field::HEIGT);
+    }
+}
